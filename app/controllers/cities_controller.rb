9b4acbf88e7035604e12159fd11d004d972883ef
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2022-03-27 04:55:55 +0200

    fetch data

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,7 +2,12 @@ class CitiesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @cities = City.all
+    # @cities = City.all
+    # @url = 'https://api.openweathermap.org/data/2.5/forecast?lang=fr&units=metric&id=524901&appid=ee281cfabf1ec4cff7712dacc69ff59f'
+    @url = 'https://api.openweathermap.org/data/2.5/onecall?lat=48.856614&lon=2.3522219&lang=fr&units=metric&exclude=minutely,alerts&appid=10dfbd8979e309323758e2b9b08bd982'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
   end
 
   def show
